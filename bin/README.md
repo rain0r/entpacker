bc7b9d109bd22e415b6b61f3cfab3eec13ec5d52
Rainer Hihn <hihn.rainer@gmail.com>
2019-02-21 09:46:54 +0100

    - Logging - Settings moved from application.properties to ~/.config/entpacker

---
+++ bin/README.md
@@ -0,0 +1,8 @@
+# entpacker
+Extract all zip-files in a directory. 
+
+Build a single, executable jar:
+
+```
+mvn clean compile assembly:single
+```
--- src/main/java/org/hihn/entpacker/Entpacker.java
+++ src/main/java/org/hihn/entpacker/Entpacker.java
@@ -1,116 +1,135 @@
 package org.hihn.entpacker;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Properties;
-
 public class Entpacker {
 
+	private boolean deleteArchive;
+	private boolean logging;
 
-    private boolean deleteArchive = false;
+	public static void main(String[] args) {
+		new Entpacker(args);
+	}
 
-    private boolean logging = false;
+	Entpacker(String[] args) {
+		loadProperties();
 
-    public static void main(String[] args) {
-        new Entpacker(args);
-    }
+		for (String pPath : args) {
+			log("Handling: " + pPath);
+			Path targetDir = new File(pPath).toPath();
+			if (Files.isDirectory(targetDir)) {
+				try {
+					scanDirForZips(targetDir);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    Entpacker(String[] args) {
-        loadProperties();
+	private void scanDirForZips(Path foo) throws IOException {
+		String glob = "glob:**/*.zip";
+		String path = foo.toString();
+		final PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(glob);
+		Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+				if (pathMatcher.matches(path)) {
+					log("Unzipping: " + path);
+					unzip(path);
+				}
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
 
-        for (String pPath : args) {
-            Path targetDir = new File(pPath).toPath();
-            if (Files.isDirectory(targetDir)) {
-                try {
-                    scanDirForZips(targetDir);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	private void unzip(Path fullZipPath) throws IOException {
+		Path prefixDir = fullZipPath.getParent();
+		String basename = fullZipPath.toFile().getName();
+		String[] tokens = basename.split("\\.(?=[^\\.]+$)");
+		if (tokens.length != 2) {
+			throw new RuntimeException();
+		}
+		String dirname = tokens[0];
+		File destDir = prefixDir.resolve(Paths.get(dirname)).toFile();
+		if (destDir.exists()) {
+			log("Dir: " + destDir + " exists. Skipping");
+			return;
+		}
+		try {
+			log("Unzipping: " + fullZipPath);
+			ZipFile zipFile = new ZipFile(fullZipPath.toString());
+			zipFile.extractAll(destDir.toString());
+			if (isDeleteArchive()) {
+				log("Deleting archive: " + fullZipPath);
+				fullZipPath.toFile().delete();
+			}
+		} catch (ZipException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void scanDirForZips(Path foo) throws IOException {
-        String glob = "glob:**/*.zip";
-        String path = foo.toString();
-        final PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(glob);
-        Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-                if (pathMatcher.matches(path)) {
-                    unzip(path);
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
-    }
+	private void loadProperties() {
+		Path path = Paths.get(getProgrammDir().toString(), "settings.ini");
+		try {
+			IniFile iniFile = new IniFile(path.toString());
+			setDeleteArchive(Boolean.valueOf(iniFile.getString("main", "delete_archive", "false")));
+			setLogging(Boolean.valueOf(iniFile.getString("main", "logging", "false")));
 
-    private void unzip(Path fullZipPath) throws IOException {
-        Path prefixDir = fullZipPath.getParent();
-        String basename = fullZipPath.toFile().getName();
-        String[] tokens = basename.split("\\.(?=[^\\.]+$)");
-        if (tokens.length != 2) {
-            throw new RuntimeException();
-        }
-        String dirname = tokens[0];
-        File destDir = prefixDir.resolve(Paths.get(dirname)).toFile();
-        if (destDir.exists()) {
-            if (isLogging()) {
-                System.out.println("Dir: " + destDir + " exists. Skipping");
-            }
-            return;
-        }
-        try {
-            if (isLogging()) {
-                System.out.println("Unzipping: " + fullZipPath);
-            }
-            ZipFile zipFile = new ZipFile(fullZipPath.toString());
-            zipFile.extractAll(destDir.toString());
-            if (isDeleteArchive()) {
-                if (isLogging()) {
-                    System.out.println("Deleting archive: " + fullZipPath);
-                }
-                fullZipPath.toFile().delete();
-            }
-        } catch (ZipException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    private void loadProperties() {
-        Properties properties = new Properties();
+	private Path getProgrammDir() {
+		return Paths.get(System.getProperty("user.home"), ".config", "entpacker");
+	}
 
-        try {
-            properties.load(Entpacker.class.getResourceAsStream("/application.properties"));
+	private void log(String txt) {
+		if (isLogging()) {
+			System.out.println(txt);
 
-            boolean deleteArchive = Boolean.valueOf(properties.getProperty("delete.archive"));
-            boolean logging = Boolean.valueOf(properties.getProperty("logging"));
+			try {
+				Path logFile = Paths.get(getProgrammDir().toString(), "entpacker.log");
 
-            setDeleteArchive(deleteArchive);
-            setLogging(logging);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+				if (!logFile.toFile().exists()) {
+					logFile.toFile().createNewFile();
+				}
+				Files.write(logFile, txt.getBytes(), StandardOpenOption.APPEND);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public boolean isDeleteArchive() {
-        return deleteArchive;
-    }
+	public boolean isDeleteArchive() {
+		return deleteArchive;
+	}
 
-    public void setDeleteArchive(boolean deleteArchive) {
-        this.deleteArchive = deleteArchive;
-    }
+	public void setDeleteArchive(boolean deleteArchive) {
+		this.deleteArchive = deleteArchive;
+	}
 
-    public boolean isLogging() {
-        return logging;
-    }
+	public boolean isLogging() {
+		return logging;
+	}
 
-    public void setLogging(boolean logging) {
-        this.logging = logging;
-    }
+	public void setLogging(boolean logging) {
+		this.logging = logging;
+	}
 }
